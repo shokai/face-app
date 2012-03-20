3b4d030e2ad6468a165a194b9d42e8d061047226
Sho Hashimoto <hashimoto@shokai.org>
2012-03-21 04:29:32 +0900

    connect mongodb

--- server/bootstrap.rb
+++ server/bootstrap.rb
@@ -3,11 +3,28 @@ require 'bundler/setup'
 require 'rack'
 require 'sinatra/reloader' if development?
 require 'sinatra/content_for'
+require 'mongoid'
+require 'yaml'
 require 'json'
 require 'haml'
 require 'sass'
 require 'fileutils'
 require 'digest/md5'
+
+begin
+  @@conf = YAML::load open(File.dirname(__FILE__)+'/config.yml').read
+  p @@conf
+rescue => e
+  STDERR.puts 'config.yaml load error!'
+  STDERR.puts e
+  exit 1
+end
+
+Mongoid.configure do |conf|
+  host = @@conf['mongo']['host']
+  port = @@conf['mongo']['port']
+  conf.master = Mongo::Connection.new.db(@@conf['mongo']['database'])
+end
 
 [:helpers, :models ,:controllers].each do |dir|
   Dir.glob(File.dirname(__FILE__)+"/#{dir}/*.rb").each do |rb|
